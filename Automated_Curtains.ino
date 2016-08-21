42d92880a35753e48fed346df8ea2973eab6ae0e
jerwil <jerwil@gmail.com>
2016-08-21 01:36:30 -0700

    Arduino responds to digital input to open
    And added schematic. Added alarm buffer so that alarm does not continue
    to cause curtain to open and close. Arduino awaits digital signal from
    ESP8266 for alarm event

--- Automated_Curtains.ino
+++ Automated_Curtains.ino
@@ -6,7 +6,12 @@
 
 double milis_timer[1] = {0}; // This is used to keep track of the timer used to tick for each milisecond
 double second_timer[1] = {0}; // This is used to keep track of the timer used to tick for each second
+double buffer_timer[1] = {0}; // This is used to keep track of the timer used to tick for each second
 unsigned long currentTime;
+word alarm_time_buffer = 7200; // If the alarm has gone off, wait until this amount of seconds have passed before it can be triggered again.
+word alarm_time_passed = 7200; // The amount of seconds since the alarm was triggered
+int pause_time = 10; // Time in seconds to pause between opening and closing
+int pause_counter = 0; // Counter to track how long the system has paused for
 
 double open_time = 9; // Time in seconds to open curtains
 double close_time = 8.2; // Time in seconds to reset string
@@ -17,17 +22,14 @@ double motor_timer = 0;
 
 const int button1Pin = 3;     // the number of the pushbutton pin
 const int button2Pin = 4;     // the number of the 2nd pushbutton pin
+const int DIPin = 12;
 
 int button1State = 0; 
 int button2State = 0; 
+int alarm_signal = 0;
+int alarm_signal_timer = 0; // Timer and target are used to make sure that alarm signal is high for at least the target time to avoid noise
+int alarm_signal_target = 2;
 
-int button1_press_initiate[1];     // storage for button press function
-int button1_press_completed[1];    // storage for button press function
-int button1_pushed;
-
-int button2_press_initiate[1];     // storage for button press function
-int button2_press_completed[1];    // storage for button press function
-int button2_pushed;
 
 
 int motor_state = 0;
@@ -49,6 +51,7 @@ void setup()
   Serial.println("Simple Motor Shield sketch");
   pinMode(button1Pin, INPUT);
   pinMode(button2Pin, INPUT);
+  pinMode(DIPin, INPUT);
 
   // Use the default "Servo" library of Arduino.
   // Attach the pin number to the servo library.
@@ -63,24 +66,43 @@ void loop()
   
     button1State = digitalRead(button1Pin);
     button2State = digitalRead(button2Pin);
-    button1_pushed = button_press(button1State, button1_press_initiate, button1_press_completed);
-    button2_pushed = button_press(button2State, button2_press_initiate, button2_press_completed);
-  
+    alarm_signal = digitalRead(DIPin);
+    
+  if (tick(1000,buffer_timer) == 1){
+    alarm_time_passed += 1;
+    if (alarm_time_passed > 32000){alarm_time_passed = alarm_time_buffer + 1;} // Avoid overflow
+    if (alarm_signal_timer >= alarm_signal_target && alarm_time_passed < alarm_time_buffer){
+      Serial.println("Alarm is sounding, but it's too soon!");
+      Serial.print(alarm_time_passed);
+      Serial.print("/");
+      Serial.print(alarm_time_buffer);
+      Serial.println(" seconds since alarm went off");
+    }
+  }
   if (mode == "idle"){
-  motor_state = 0;
-  if (button1_pushed == 1) {
+    if (button1State == HIGH && button2State == LOW) {
+      motor_state = 1;
+      Serial.println("Forward!");
+    }
+    else if(button2State == HIGH && button1State == LOW){
+      motor_state = -1;
+      Serial.println("Backward!");
+    }
+    else{
+      motor_state = 0;
+    }
+  if (tick(1000,second_timer) == 1){
+  if (alarm_signal == 1) {    
+      alarm_signal_timer +=1;
+  }
+  else {alarm_signal_timer = 0;}
+  }
+    if (alarm_signal_timer >= alarm_signal_target && alarm_time_passed > alarm_time_buffer){
     mode = "open";
     Serial.println("Open");
-    button1_pushed = 0;
+    alarm_signal_timer = 0;
     }
-  else if(button2_pushed == 1){
-    mode = "close";
-    Serial.println("Close");
-    button2_pushed = 0;
-  }
-  else {
-    mode = "idle";
-  }
+    
   }
   
   else if (mode == "open"){
@@ -91,15 +113,36 @@ void loop()
       Serial.print("/");
       Serial.println(open_time);
     }
-    if (motor_timer >= open_time || button1_pushed == 1 || button2_pushed == 1){
+    if (motor_timer >= open_time){ 
+      mode = "pause";
+      motor_timer = 0;
+      Serial.println("Pausing");
+    }
+    else if (button1State == HIGH || button2State == HIGH){ // Allow user to interrupt automatic opening
       mode = "idle";
       motor_timer = 0;
-      Serial.println("Idle State");
-      button1_pushed = 0;
-      button2_pushed = 0;
+      Serial.println("interrupted by user");
     }
     motor_state = 1;
+    pause_counter = 0;
+    alarm_time_passed = 0;
   }
+  
+  else if (mode == "pause"){
+    motor_state = 0;
+    if (tick(1000,milis_timer) == 1){
+      pause_counter += 1;
+      Serial.print("Pause timer: ");
+      Serial.print(pause_counter);
+      Serial.print("/");
+      Serial.println(pause_time);
+    }
+    if (pause_counter >= pause_time){
+      mode = "close";
+      pause_counter = 0;
+    }
+  }
+  
   else if (mode == "close"){
     if (tick(100,milis_timer) == 1){
       motor_timer += .10;
@@ -108,12 +151,10 @@ void loop()
       Serial.print("/");
       Serial.println(close_time);
     }
-    if (motor_timer >= close_time || button1_pushed == 1 || button2_pushed == 1){
+    if (motor_timer >= close_time || button1State == HIGH || button2State == HIGH){
       mode = "idle";
       motor_timer = 0;
       Serial.println("Idle State");
-      button1_pushed = 0;
-      button2_pushed = 0;
     }
     motor_state = -1;
   }
@@ -142,18 +183,5 @@ if(currentTime >= (timekeeper[0] + delay)){
   return 1;
   }
 else {return 0;}
-}
-
-int button_press (int button_indicator, int button_press_initiated[1], int button_press_complete[1]){
-  if (button_indicator == 0 && button_press_initiated[0] == 1) {
-  button_press_complete[0] = 1;
-  button_press_initiated[0] = 0;
-  }
-  else if (button_indicator == 1){
-  button_press_initiated[0] = 1;
-  button_press_complete[0] = 0;
-  }
-  else {button_press_complete[0] = 0;}
-return button_press_complete[0];
 }
 
